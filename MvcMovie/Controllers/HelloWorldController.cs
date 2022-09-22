7071975949e34736ffa61d65a4611d185f6e832f
Prabin Bhagat <prabin.bhagat@gmail.com>
2022-09-22 07:52:55 +0530

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //GET: /Hello World/      ----its also the class name of this controller
+
+        public string Index()
+        {
+            string name = "Prabin Bhagat";
+            return "This is my Default action....i.e. Index " + "Hello " + name;
+        }
+
+        //GET: /HelloWorld/Welcome/
+
+        public string Welcome()
+        {
+            return "This is Welcome action method....";
+        }
+
+
+        //<------------Default is commented below------------>
+        //public IActionResult Index()  
+        //{
+        //    return View();
+        //}
+        //</------------------------------------------------->
+    }   
+}
